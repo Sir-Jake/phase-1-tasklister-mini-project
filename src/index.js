4fc1eae871eb61afa04ab4dbc570c8884a535a8a
Sir JAke <sir.johnjake@gmail.com>
2025-12-24 20:19:01 +0300

    Tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const createTaskForm = document.querySelector("#create-task-form");
+const taskList = document.querySelector("#tasks");
+
+createTaskForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const newTaskDescriptionInput = document.querySelector(
+    "#new-task-description"
+  );
+  const taskDescription = newTaskDescriptionInput.value;
+
+  function buildToDo(task) {
+    const li = document.createElement("li");
+    li.textContent = task;
+    taskList.appendChild(li);
+  }
+
+  buildToDo(taskDescription);
+
+  // Optional: Clear input after submission
+  newTaskDescriptionInput.value = "";
 });
